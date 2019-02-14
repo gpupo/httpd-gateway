48b9907d4be1f06ff93e1742bd624b47f88dadb7
Gilmar Pupo <126154+gpupo@users.noreply.github.com>
2019-02-14 14:13:42 -0200

    Add log cleanup

---
+++ bin/log-cleanup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+source "$(dirname "$0")/common/bootstrap.sh"
+
+log_cleanup() {
+    du -sh $1; actualsize=$(wc -c <"$1");
+    test $actualsize -ge 58206070 && echo '' > $1;
+}
+
+printf "Cleanup Docker logs\n";
+for f in /var/lib/docker/containers/*/*-json.log; do \
+  log_cleanup "$f"; \
+done
+
+
+printf "Done\n";
