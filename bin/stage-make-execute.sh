101c374d8f3f5774d91b6dae6213707e296a50ef
Gilmar Pupo <126154+gpupo@users.noreply.github.com>
2019-02-20 10:38:21 -0300

    Improve log

--- bin/stage-make-execute.sh
+++ bin/stage-make-execute.sh
@@ -2,7 +2,11 @@
 source "$(dirname "$0")/common/bootstrap.sh"
 TARGET_DIRECTORY="${STAGE_DIRECTORY}/$1/current"
 LOGS_DIRECTORY="${STAGE_DIRECTORY}/$1/logs"
+LOG_FILE=${LOGS_DIRECTORY}/${2}.log;
 printf "\nStage $1 :: make $2";
 cd ${TARGET_DIRECTORY};
-/usr/local/bin/docker-compose exec -T php-fpm bash -c "make $2" >> ${LOGS_DIRECTORY}/$2 2>&1;
+
+printf "\n= = = = = = = = = = = = = = =\n$(date)\n" >> ${LOG_FILE};
+/usr/local/bin/docker-compose exec -T php-fpm bash -c "make $2" >> ${LOG_FILE} 2>&1;
+
 printf " ... done.\n";
