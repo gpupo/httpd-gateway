db84b173189f4a172b62e7ec5da4f32495347fb6
Gilmar Pupo <126154+gpupo@users.noreply.github.com>
2020-10-28 15:24:51 -0300

    fix up

--- bin/up-stages.sh
+++ bin/up-stages.sh
@@ -7,10 +7,10 @@ CURRENT_STAGE_NAME=$1;
 
 if [ -z "$CURRENT_STAGE_NAME" ]
 then
-    up_stage "${CURRENT_STAGE_NAME}";
-else
   eval $(grep_builder_line $SERVER_STAGE_CONFIG_DIR_PATH) | while read CURRENT_STAGE_NAME
   do
     up_stage "${CURRENT_STAGE_NAME}";
   done
+else
+    up_stage "${CURRENT_STAGE_NAME}";
 fi;
