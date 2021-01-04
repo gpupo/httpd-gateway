7e4ace196f1270b7c4cf2d4721a6142ed8fa75e7
Gilmar Pupo <126154+gpupo@users.noreply.github.com>
2021-01-04 17:59:50 -0300

    HTTPS_METHOD=noredirect

--- bin/make-file/targets/02-maintence.mk
+++ bin/make-file/targets/02-maintence.mk
@@ -4,3 +4,14 @@ maintence@pull:
 	$(DCC) pull nginx-proxy-letsencrypt
 	printf "${COLOR_COMMENT}Done${COLOR_RESET}\n"
 
+## View Last logs
+maintence@logs:
+	$(DCC) logs --tail=200;
+	printf "${COLOR_COMMENT}Done${COLOR_RESET}\n"
+
+## Reboot only gateway services
+maintence@reboot:
+	$(DCC) down;
+	$(DCC) up -d;
+	printf "${COLOR_COMMENT}Done${COLOR_RESET}\n"
+
