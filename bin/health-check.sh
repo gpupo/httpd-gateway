32de5e4da1b5a130c73271836c56165b55a5a591
Gilmar Pupo <126154+gpupo@users.noreply.github.com>
2021-07-01 16:39:32 -0300

    Reduz traefik

--- bin/health-check.sh
+++ bin/health-check.sh
@@ -6,17 +6,20 @@ make basic;
 /usr/local/bin/docker-compose logs --tail=5 -f &
 
 sleep 10;
+curl localhost;
 curl -H "Host:whoami.local" localhost;
 docker ps;
 curl "https://${SERVER_DEFAULT_HOST:-whoami.yourdomain.tld}"
 
 
-
 printf "\n Test Traefik\n"
 curl -H "Host:traefik.local" localhost;
 
+printf "\n Test Traefik API"
+curl http://127.0.0.1:808o;
+
 printf "\n Test Traefik container (PORT)\n"
-curl -H "Host:whoami-backend.place" localhost:8080;
+curl -H "Host:whoami-backend.place" http://127.0.0.1:8081;
 
 printf "\n Test Traefik container\n"
 curl -H "Host:whoami-backend.place" localhost;
