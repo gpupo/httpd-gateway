9c8ce754700364d8f9dd92dceb73095bc0b70c90
Gilmar Pupo <126154+gpupo@users.noreply.github.com>
2021-07-01 20:52:24 -0300

    Simplifica teste

--- bin/health-check.sh
+++ bin/health-check.sh
@@ -5,21 +5,21 @@ git pull;
 make basic;
 /usr/local/bin/docker-compose logs --tail=5 -f &
 
-printf "\n whoami local\n"
+printf "\n> Test whoami local\n"
 curl -H "Host:whoami.local" localhost;
 
-printf "\n whoami ssl\n"
+printf "\n> Test whoami ssl\n"
 curl "https://${SERVER_DEFAULT_HOST:-whoami.yourdomain.tld}"
 
 
-printf "\n Test Traefik\n"
+printf "\n> Test Traefik\n"
 curl -H "Host:traefik.place" localhost;
 
-printf "\n Test Traefik API"
-curl http://127.0.0.1:8080;
+# printf "\n Test Traefik API"
+# curl http://127.0.0.1:8080;
 
-printf "\n Test Traefik container (PORT)\n"
-curl -H "Host:whoami-backend.place" http://127.0.0.1:8081;
+# printf "\n Test Traefik container (PORT)\n"
+# curl -H "Host:whoami-backend.place" http://127.0.0.1:8081;
 
 printf "\n Test Traefik container\n"
 curl -H "Host:whoami-backend.place" localhost;
