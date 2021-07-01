bc0e43b0ff92d0c17eced80f8a2573c7db20e8e9
Gilmar Pupo <126154+gpupo@users.noreply.github.com>
2021-07-01 15:46:13 -0300

    Add logs

--- bin/health-check.sh
+++ bin/health-check.sh
@@ -3,9 +3,17 @@ source "$(dirname "$0")/common/bootstrap.sh"
 make stop;
 git pull;
 make basic;
+/usr/local/bin/docker-compose logs --tail=5 -f &
+
 sleep 10;
 curl -H "Host: whoami.local" localhost;
 docker ps;
 curl "https://${SERVER_DEFAULT_HOST:-whoami.yourdomain.tld}"
+
+
+
+printf "\n Test Traefik\n"
 curl -H "Host: traefik.local" localhost;
+
+printf "\n Test Traefik container\n"
 curl -H "Host: whoami-backend.place" localhost;
