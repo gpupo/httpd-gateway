44ae86f5515bc5518c8d232808bdf5afd15299ef
Gilmar Pupo <126154+gpupo@users.noreply.github.com>
2021-07-01 14:30:16 -0300

    Reduz vlumes

--- bin/health-check.sh
+++ bin/health-check.sh
@@ -3,5 +3,5 @@ source "$(dirname "$0")/common/bootstrap.sh"
 make stop;
 git pull;
 make alone;
-sleep 30;
-curl --location --request GET localhost --header 'Host: whoami.docker.place'
+sleep 10;
+curl -H "Host: whoami.local" localhost
