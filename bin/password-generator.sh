32857f63ab1563a8e326aa6a6a4a614bd0978651
Gilmar Pupo <126154+gpupo@users.noreply.github.com>
2021-07-02 14:36:44 -0300

    add format

--- bin/password-generator.sh
+++ bin/password-generator.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 source "$(dirname "$0")/common/bootstrap.sh"
-printf "\n> bcrypt password generate\n"
-htpasswd -nbB "${1:-myname}" "${2:-myPassword}";
+# printf "\n> bcrypt password generate\n"
+NEW_HASH=$(htpasswd -nbB "${1:-myname}" "${2:-myPassword}");
+printf "\"%s\"," "$NEW_HASH";
