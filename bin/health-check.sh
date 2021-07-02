aff8024bde055af9ff6081ef9b7a0a9e2d76c1b0
Gilmar Pupo <126154+gpupo@users.noreply.github.com>
2021-07-02 14:30:42 -0300

    Add password generator

--- bin/health-check.sh
+++ bin/health-check.sh
@@ -4,7 +4,12 @@ source "$(dirname "$0")/common/bootstrap.sh"
 printf "\n> Test whoami ssl\n"
 curl "https://${SERVER_DEFAULT_HOST}"
 
+printf "\n> Test Traefik backend container without AUTH\n";
+curl  localhost \
+--header 'Host: whoami-backend.place'
+printf "\n Expected: 404\n";
+
 printf "\n Test Traefik container\n";
 curl  localhost \
 --header 'Host: whoami-backend.place' \
---header 'Authorization: Basic Zm9vOmJhcg==';
+--header 'Authorization: Basic ${PROXY_HEALTH_CHECK_AUTH_TOKEN:-Zm9vOmJhcg==}';
