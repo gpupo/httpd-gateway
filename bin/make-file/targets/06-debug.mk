8c42eb20f7258ab771975ecd75b7d6a7c28fe3b5
Gilmar Pupo <126154+gpupo@users.noreply.github.com>
2021-07-13 09:12:35 -0300

    Add acme conf

--- bin/make-file/targets/06-debug.mk
+++ bin/make-file/targets/06-debug.mk
@@ -1,4 +1,4 @@
 ## DEBUG nginx-proxy
 debug@nginx-proxy:
-	$(DCC) exec nginx-proxy cat /etc/nginx/conf.d/default
+	$(DCC) exec nginx-proxy cat /etc/nginx/vhost.d/*
 	printf "${COLOR_COMMENT}Done${COLOR_RESET}\n"
