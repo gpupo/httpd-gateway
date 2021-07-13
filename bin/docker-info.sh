2d2c91b55ca02699ada6ed5171843dfff0cb62f2
Gilmar Pupo <126154+gpupo@users.noreply.github.com>
2021-07-13 10:19:28 -0300

    Add backend ip

--- bin/docker-info.sh
+++ bin/docker-info.sh
@@ -5,23 +5,23 @@ function inspect_docker_containers_network() {
     _print_container_network_info() {
         local container_id
         local container_ports
-        local container_ip
+        local container_frontend_ip
         local container_name
         container_id="${1}"
     
         container_ports=( $(docker port "$container_id" | grep -o "0.0.0.0:.*" | cut -f2 -d:) )
         container_name="$(docker inspect --format "{{ .Name }}" "$container_id" | sed 's/\///')"
-        container_ip="$(docker inspect --format "{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}} " "$container_id")"
-        printf "%-40s %-13s %-20s\n" "$container_name" "$container_id" "$container_ip"
+        container_frontend_ip="$(docker inspect --format "{{ .NetworkSettings.Networks.frontendNetwork.IPAddress }}" "$container_id")"
+        container_backend_ip="$(docker inspect --format "{{ .NetworkSettings.Networks.backendNetwork.IPAddress }}" "$container_id")"
+        printf "%-40s %-13s %-20s %-20s\n" "$container_name" "$container_id" "$container_frontend_ip" "$container_backend_ip"
     }
 
 
-    printf "%-40s %-13s %-20s\n" 'Container Name' 'Container Id' 'Container IP'
+    printf "%-40s %-13s %-20s %-20s\n" 'Container Name' 'Container Id' 'Frontend IP' 'Backend IP'
     local container_id
     docker ps -a --format "{{.ID}}" | while read -r container_id ; do
         _print_container_network_info  "$container_id"
     done
-
 }
 
 inspect_docker_containers_network;
