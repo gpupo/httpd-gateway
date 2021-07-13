9cce5956eb7a8cb86b53ef28e35f9cae1b68223e
Gilmar Pupo <126154+gpupo@users.noreply.github.com>
2021-07-13 18:19:38 -0300

    Remove traefik env var

--- bin/make-file/targets/06-debug.mk
+++ bin/make-file/targets/06-debug.mk
@@ -1,4 +1,4 @@
 ## DEBUG nginx-proxy
 debug@nginx-proxy:
-	$(DCC) exec nginx-proxy cat /etc/nginx/conf.d/default.conf
+	$(DCC) exec nginx-proxy cat /etc/nginx/conf.d/default.conf;
 	printf "${COLOR_COMMENT}Done${COLOR_RESET}\n"
