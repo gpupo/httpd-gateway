91cb6f710d7d04236fddbb4889b7d4ccfc86a214
Gilmar Pupo <126154+gpupo@users.noreply.github.com>
2021-07-13 10:08:08 -0300

    Add script

---
+++ bin/docker-info.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+source "$(dirname "$0")/common/bootstrap.sh"
+
+function inspect_docker_containers_network() {
+    _print_container_network_info() {
+        local container_id
+        local container_ports
+        local container_ip
+        local container_name
+        container_id="${1}"
+    
+        container_ports=( $(docker port "$container_id" | grep -o "0.0.0.0:.*" | cut -f2 -d:) )
+        container_name="$(docker inspect --format "{{ .Name }}" "$container_id" | sed 's/\///')"
+        container_ip="$(docker inspect --format "{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}" "$container_id")"
+        printf "%-13s %-40s %-20s %-80s\n" "$container_id" "$container_name" "$container_ip" "${container_ports[*]}"
+    }
+
+    local container_id
+    container_id="$1"
+    printf "%-13s %-40s %-20s %-80s\n" 'Container Id' 'Container Name' 'Container IP' 'Container Ports'
+    if [ -z "$container_id" ]; then
+        local container_id
+        docker ps -a --format "{{.ID}}" | while read -r container_id ; do
+            _print_container_network_info  "$container_id"
+        done
+    else
+        _print_container_network_info  "$container_id"
+    fi
+}
+
+inspect_docker_containers_network;
