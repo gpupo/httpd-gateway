d9e71761927cd96880870ce604565b7e593c1b87
Gilmar Pupo <126154+gpupo@users.noreply.github.com>
2021-07-13 11:48:44 -0300

    Add warmup domain

---
+++ bin/domain-warmup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+source "$(dirname "$0")/common/bootstrap.sh"
+    # --name warmup-${WARMUP_DOMAIN} \
+
+WARMUP_DOMAIN=${1:-}
+docker run \
+    -d \
+    --expose 8080 \
+    -e VIRTUAL_HOST=${WARMUP_DOMAIN} \
+    -e LETSENCRYPT_HOST=${WARMUP_DOMAIN} \
+    -e VIRTUAL_PORT=8000 \
+    -e DEBUG=true \
+    --network="frontendNetwork" \
+ jwilder/whoami
--- bin/make-file/targets/06-debug.mk
+++ bin/make-file/targets/06-debug.mk
@@ -1,4 +1,4 @@
 ## DEBUG nginx-proxy
 debug@nginx-proxy:
-	$(DCC) exec nginx-proxy cat /etc/nginx/vhost.d/*
+	$(DCC) exec nginx-proxy cat /etc/nginx/conf.d/default.conf
 	printf "${COLOR_COMMENT}Done${COLOR_RESET}\n"
