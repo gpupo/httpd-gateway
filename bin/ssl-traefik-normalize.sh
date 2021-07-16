507b8076a6124edce9622539cd92945b402299d4
Gilmar Pupo <126154+gpupo@users.noreply.github.com>
2021-07-16 08:08:29 -0300

    Add ssl normalizer

---
+++ bin/ssl-traefik-normalize.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+source .env;
+
+symlinks_default_cert_to_dot_place() {
+    local file_prefix=${1};
+    local source_file=/etc/nginx/certs/${SERVER_DEFAULT_HOST}.${file_prefix};
+    local target_file=/etc/nginx/certs/.place.${file_prefix};
+    printf "Symlink %s => %s\n" "${source_file}" "${target_file}";
+    docker-compose exec nginx-proxy "lnf -sn ${source_file} ${target_file}";
+}
+
+symlinks_default_cert_to_dot_place "crt";
+symlinks_default_cert_to_dot_place "key";
+symlinks_default_cert_to_dot_place "dhparam.pem";
+symlinks_default_cert_to_dot_place "chain.pem";
